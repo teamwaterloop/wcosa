00d89cff6cffcd17fb33fe5aa8f6f2c50b388e25
Jeff Niu <jeffniu22@gmail.com>
2017-12-20 23:55:43 -0800

    Finding variants and cores

--- toolchain/cmake/Platform/Initialization/CosaRegisterHardwarePlatform.cmake
+++ toolchain/cmake/Platform/Initialization/CosaRegisterHardwarePlatform.cmake
@@ -13,11 +13,6 @@ find_file(COSA_VARIANTS_PATH
         PATHS ${COSA_SDK_PATH}
         DOC "Path to directory containing the Cosa variant sources.")
 
-find_file(COSA_VARIANTS_PATH
-        NAMES arduino
-        PATHS ${COSA_VARIANTS_PATH}
-        DOC "Path to directory containing the Cosa variant sources.")
-
 find_file(COSA_BOOTLOADERS_PATH
         NAMES bootloaders
         PATHS ${COSA_SDK_PATH}
@@ -27,6 +22,9 @@ find_file(COSA_BOARDS_PATH
         NAMES boards.txt
         PATHS ${COSA_SDK_PATH}
         DOC "Path to Cosa boards definition file.")
+
+# Cosa variants are located under `variants/arduino`
+set(COSA_VARIANTS_PATH ${COSA_VARIANTS_PATH}/arduino)
 
 message(STATUS "Founds paths")
 message(STATUS "Cores:       ${COSA_CORES_PATH}")
@@ -38,7 +36,7 @@ if (NOT COSA_CORES_PATH OR NOT EXISTS ${COSA_CORES_PATH})
     message(FATAL_ERROR "Failed to find COSA_CORES_PATH to `cores`")
 endif ()
 if (NOT COSA_VARIANTS_PATH OR NOT EXISTS ${COSA_VARIANTS_PATH})
-    message(FATAL_ERROR "Failed to find COSA_VARIANTS_PATH to `variants`")
+    message(FATAL_ERROR "Failed to find COSA_VARIANTS_PATH to `variants/arduino`")
 endif ()
 if (NOT COSA_BOOTLOADERS_PATH OR NOT EXISTS ${COSA_BOOTLOADERS_PATH})
     message(FATAL_ERROR "Failed to find COSA_BOOTLOADERS_PATH to `bootloaders`")
@@ -62,7 +60,39 @@ unset(example_board_1)
 unset(example_board_2)
 
 # Read in variant boards
+message(STATUS "Reading variants from directory")
 file(GLOB variant_sub_dir ${COSA_VARIANTS_PATH}/*)
+unset(COSA_VARIANTS CACHE)
 foreach (variant_dir ${variant_sub_dir})
-    message(${variant_dir})
+    if (IS_DIRECTORY ${variant_dir})
+        get_filename_component(variant ${variant_dir} NAME)
+        set(COSA_VARIANTS ${COSA_VARIANTS} ${variant} CACHE INTERNAL "A list of registered variants")
+        set(${variant}.path ${variant_dir} CACHE INTERNAL "The path to variant ${variant}")
+        if (WCOSA_DEBUG)
+            message(STATUS "Variant [${variant}]: ${${variant}.path}")
+        endif ()
+    endif ()
 endforeach ()
+list(LENGTH COSA_VARIANTS length_cosa_variants)
+message(STATUS "Found and cached ${length_cosa_variants} variants")
+unset(variant_sub_dir)
+unset(length_cosa_variants)
+
+# Read in cores
+message(STATUS "Reading cores from directory")
+file(GLOB cores_sub_dir ${COSA_CORES_PATH}/*)
+unset(COSA_CORES CACHE)
+foreach (core_dir ${cores_sub_dir})
+    if (IS_DIRECTORY ${core_dir})
+        get_filename_component(core ${core_dir} NAME)
+        set(COSA_CORES ${COSA_CORES} ${core} CACHE INTERNAL "A list of registered cores")
+        set(${core}.path ${core_dir} CACHE INTERNAL "The path to core ${core}")
+        if (WCOSA_DEBUG)
+            message(STATUS "Core [${core}]: ${${core}.path}")
+        endif ()
+    endif ()
+endforeach ()
+list(LENGTH COSA_CORES length_cosa_cores)
+message(STATUS "Found and cached ${length_cosa_cores} cores")
+unset(cores_sub_dir)
+unset(length_cosa_cores)
