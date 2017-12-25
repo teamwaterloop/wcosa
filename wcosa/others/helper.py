7f4f01b82e54e70504ae4d6e9a936039baa53520
Deep Dhillon <deep.dhill6@gmail.com>
2017-12-25 12:21:09 -0500

    fixed paths and rearranged structure to allow for better deployment

--- wcosa/others/helper.py
+++ wcosa/others/helper.py
@@ -40,10 +40,25 @@ def get_working_directory():
 
 def get_wcosa_path():
     """returns the absolute path of wcosa"""
-    return linux_path(os.path.abspath("./../../"))
+
+    return linux_path(os.path.abspath(sys.path[0] + "/.."))
+
+
+def get_cosa_path():
+    """returns the absolute path of cosa"""
+
+    return linux_path(get_wcosa_path() + "/toolchain/cosa")
+
+
+def get_settings_path():
+    """returns the absolute path of settings file"""
+
+    return linux_path(get_wcosa_path() + "/wcosa/settings.json")
 
 
 def get_working_directory():
+    """get path from where the script is called"""
+
     return linux_path(os.getcwd())
 
 
