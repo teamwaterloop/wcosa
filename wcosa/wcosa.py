09a5cd63aabe77cead20c4a6358032700873fa49
Deep Dhillon <deep.dhill6@gmail.com>
2017-12-25 20:52:28 -0500

    created build, upload and clean. other minor fixes and updates

--- wcosa/wcosa.py
+++ wcosa/wcosa.py
@@ -1,12 +1,16 @@
+"""
+Main Script that calls other scripts to make wcosa work
+"""
+
 import argparse
+
 import handle
-from colorama import Fore
-from parsers import board_parser
-from utils import output, helper
+import use
+from objects.objects import *
 
 
 def parse():
-    """Adds command line arguments and parses them"""
+    """Adds command line arguments and returns the options"""
 
     parser = argparse.ArgumentParser(description="WCosa create, build and upload Cosa AVR projects")
 
@@ -15,26 +19,16 @@ def parse():
                         type=str)
     parser.add_argument('--port', help='port to upload the AVR traget to (default: automatic)',
                         type=str)
-    parser.add_argument('--programmer', help='port to upload the AVR traget to (default: usbtinyisp)',
-                        type=str)
     parser.add_argument('--baud', help='buad rate for serial (default: 9600)',
                         type=int)
     parser.add_argument('--ide', help='create specific project structure for specific ide (default: none)',
                         type=str)
     parser.add_argument('--path', help='path to create the project at (default: curr dir)',
                         type=str)
+    parser.add_argument('--generator', help='makefile generator to use for build (default: Unix Makefiles)',
+                        type=str)
 
     return parser.parse_args()
-
-
-def verify_board(board):
-    """Verify if the board provided is supported by wcosa"""
-
-    boards = board_parser.get_all_board(helper.get_wcosa_path() + "/wcosa/boards.json")
-
-    if board is not None and board not in boards:
-        output.writeln("Board Invalid. Run wcosa script with boards option to see all the valid boards", Fore.RED)
-        quit(2)
 
 
 def print_boards():
@@ -44,25 +38,55 @@ def print_boards():
 
     output.writeln("Boards compatible with this project are: ", Fore.CYAN)
 
-    for board in boards:
-        name = board_parser.get_board_properties(board, helper.get_wcosa_path() + "/wcosa/boards.json")["name"]
-        output.writeln('{:15s} --->\t{}'.format(board, name))
+    for curr_board in boards:
+        name = board_parser.get_board_properties(curr_board, helper.get_wcosa_path() + "/wcosa/boards.json")["name"]
+        output.writeln('{:15s} --->\t{}'.format(curr_board, name))
+
+
+def provided(*args):
+    """checks if give flags are specified during command line usage"""
+
+    if any(flag is not None for flag in args):
+        return True
 
 
 if __name__ == "__main__":
     options = parse()
 
+    board = Board(options.board)
+    ide = IDE(options.ide)
+    port = Port(options.port)
+    path = Path(options.path)
+    generator = Generator(options.generator)
+
     # based on the action call scripts
     if options.action == "boards":
         print_boards()
     elif options.action == "create":
+        if provided(options.port, options.generator, options.baud):
+            output.writeln("Create only requires path, board and ide, other flags are ignored", Fore.YELLOW)
+
         if options.board is not None:
-            # verify the board and create
-            verify_board(options.board)
-            handle.create_wcosa(options.path, options.board, options.ide)
+            handle.create_wcosa(path, board, ide)
         else:
             output.writeln("Board is needed for creating wcosa project", Fore.RED)
     elif options.action == "update":
-        # verify the board and update
-        verify_board(options.board)
-        handle.update_wcosa(options.path, options.board)
+        if provided(options.port, options.generator, options.baud):
+            output.writeln("Update only requires path, board and ide, other flags are ignored", Fore.YELLOW)
+
+        handle.update_wcosa(path, board, ide)
+    elif options.action == "build":
+        if provided(options.port, options.ide, options.board, options.baud):
+            output.writeln("Build only requires path and generator, other flags are ignored", Fore.YELLOW)
+
+        use.build_wcosa(path, generator)
+    elif options.action == "upload":
+        if provided(options.ide, options.board, options.generator, options.baud):
+            output.writeln("Upload only requires path and port, other flags are ignored", Fore.YELLOW)
+
+        use.upload_wcosa(path, port)
+    elif options.action == "clean":
+        if provided(options.ide, options.board, options.generator, options.port, options.baud):
+            output.writeln("Clean only requires path, other flags are ignored", Fore.YELLOW)
+
+        use.clean_wcosa(path)
