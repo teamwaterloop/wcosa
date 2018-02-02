458c789f2310c3b324458189603ac3fcb9898837
Dmytro Shynkevych <dm.shynk@gmail.com>
2018-02-01 19:03:19 -0500

    Rudimentary install functionality implemented

--- wcosa/command/package_manager.py
+++ wcosa/command/package_manager.py
@@ -1,5 +1,6 @@
 import git
 import re
+import os, sys
 from wcosa.utils.output import write, writeln
 
 class Package:
@@ -22,8 +23,8 @@ class PackageFormatError(Exception):
         return "Bad package format: " + self.package_string
 
 FULL_URL = r'(?P<url>https?://\S+/(?P<name>\S+))'
-GITHUB = r'(?P<github>\w+/(?P<name>\w+))'
-BRANCH = r'(:(?P<branch>\w+))?'
+GITHUB = r'(?P<github>[\w\-]+/(?P<name>[\w\-]+))'
+BRANCH = r'(:(?P<branch>[\w\-]+))?'
 VERSION = r'@(?P<version>\S+)'
 PATH = r'( as (?P<path>\S+))?'
 VALID_SCHEMAS = [re.compile('^' + FULL_URL + BRANCH + VERSION + PATH + '$'),
@@ -59,3 +60,43 @@ def parse_package_names(package_strings):
         path = groups['path']
         packages.append(Package(name, url, branch, version, path))
     return packages
+
+def package_install(path, packages):
+    packages = parse_package_names(packages)
+    pkgpath = path + '/.pkg'
+
+    try:
+        pkgrepo = git.Repo(pkgpath)
+    except:
+        writeln("Initializing package repository")
+        pkgrepo = git.Repo.init(pkgpath)
+    for package in packages:
+        write("Installing %s... " % package.name)
+        sys.stdout.flush()
+        try:
+            sm = pkgrepo.create_submodule(package.name, package.name,
+                                          url=package.url, branch=package.branch)
+            # FIXME: this is very slow. Do we need this versioning functionality?
+            # sm.repo.git.remote('add', 'origin', package.url)
+            # sm.repo.git.fetch('--tags')
+        except Exception as e:
+            writeln("Package install failed: " + str(e))
+        if package.version in sm.repo.tags:
+            pass
+            # FIXME: Checks out into pkgpath
+            # sm.repo.git.checkout(package.version)
+        else:
+            writeln("No such version: %s. Checking out HEAD" % package.version)
+        pkgrepo.index.commit("Installed " + package.name)
+        try:
+            installpath = os.path.abspath(pkgpath + '/' + package.name)
+            linkpath = path + '/' + package.path
+            linkbasedir = '/'.join(linkpath.split('/')[:-1])
+            try:
+                os.mkdir(linkbasedir)
+            except:
+                pass
+            os.symlink(installpath, linkpath)
+        except Exception as e:
+            writeln("Could not link package to path: " + str(e))
+        writeln("Installed.")
