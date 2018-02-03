62ab064832cc3333d81079185da09e34c4325c69
Dmytro Shynkevych <dm.shynk@gmail.com>
2018-02-03 15:43:51 -0500

    Implemented repo initialization and package info writing

--- wcosa/command/package_manager.py
+++ wcosa/command/package_manager.py
@@ -1,4 +1,9 @@
+"""
+A lightweight package management system for WCosa.
+*_list functions operate on lists of data (common scenario, better efficiency).
+"""
 import git
+import json
 import re
 import os, sys
 from wcosa.utils.output import write, writeln
@@ -25,20 +30,20 @@ class PackageFormatError(Exception):
 FULL_URL = r'(?P<url>https?://\S+/(?P<name>\S+))'
 GITHUB = r'(?P<github>[\w\-]+/(?P<name>[\w\-]+))'
 BRANCH = r'(:(?P<branch>[\w\-]+))?'
-VERSION = r'@(?P<version>\S+)'
+VERSION = r'(@(?P<version>\S+))?'
 PATH = r'( as (?P<path>\S+))?'
 VALID_SCHEMAS = [re.compile('^' + FULL_URL + BRANCH + VERSION + PATH + '$'),
                  re.compile('^' + GITHUB + BRANCH + VERSION + PATH + '$')]
 
-def parse_package_names(package_strings):
+def package_name_parse_list(package_strings):
     """
     Convert package strings to package entities.
-    A package string is of the form '(BASE_URL|GITHUB)[:BRANCH]@VERSION as PATH'
+    A package string is of the form (BASE_URL|GITHUB)[:BRANCH][@VERSION] as PATH
     where:
         FULL_URL is a valid URL pointing to a git repository
         GITHUB is of the form 'username/reponame'
         BRANCH [default master] is the branch to track
-        VERSION [default HEAD] is a tag on the given repository
+        VERSION [default HEAD] is a tag on the given branch
         PATH is the relative path to install location
     """
     packages = []
@@ -61,22 +66,66 @@ def parse_package_names(package_strings):
         packages.append(Package(name, url, branch, version, path))
     return packages
 
-def package_install(path, packages):
-    packages = parse_package_names(packages)
-    pkgpath = path + '/.pkg'
+def package_info_write_list(pkgpath, packages):
+    if not packages:
+        return # Nothing to write
+    repo = package_repo_open(pkgpath)
+    newentries = []
+    updentries = []
+    with open(pkgpath + '/pkglist', 'r+') as pkglistfile:
+        pkglist = json.loads(pkglistfile.read())
+        pkgnames = list(map(lambda x: x['name'], pkglist))
+        for package in packages:
+            if package.name in pkgnames:
+                index = pkgnames.index(package.name)
+                if pkglist[index] == package.__dict__: # If same info present
+                    continue
+                pkglist[index] = package.__dict__
+                updentries.append(package.name)
+            else:
+                pkglist.append(package.__dict__)
+                newentries.append(package.name)
+        pkglistfile.seek(0)
+        pkglistfile.write(json.dumps(pkglist))
+    repo.index.add(['pkglist'])
+    repo.index.commit("Updated package list\n\n"
+                      + ("New: %s\n" % ', '.join(newentries)
+                          if newentries else "")
+                      + ("Updated: %s\n" % ', '.join(updentries)
+                          if updentries else ""))
 
+def package_repo_open(pkgpath):
     try:
-        pkgrepo = git.Repo(pkgpath)
-    except:
-        writeln("Initializing package repository")
-        pkgrepo = git.Repo.init(pkgpath)
+        return git.Repo(pkgpath)
+    except: # If could not open, initialize
+        return package_repo_init(pkgpath)
+
+def package_repo_init(pkgpath):
+    write("Initializing package repository... ")
+    sys.stdout.flush()
+    pkgrepo = git.Repo.init(pkgpath)
+
+    with open(pkgpath + '/pkglist', 'w+') as pkglist:
+        pkglist.write('[]') # Start with empty package list
+
+    pkgrepo.index.add(['pkglist'])
+    pkgrepo.index.commit("Initialized repository")
+    writeln("Done")
+
+    return pkgrepo
+
+def package_install_list(path, packages):
+    packages = package_names_parse(packages)
+    pkgpath = path + '/.pkg'
+    pkgrepo = package_repo_open(pkgpath)
+
     for package in packages:
         write("Installing %s... " % package.name)
         sys.stdout.flush()
         try:
             sm = pkgrepo.create_submodule(package.name, package.name,
                                           url=package.url, branch=package.branch)
-            # FIXME: this is very slow. Do we need this versioning functionality?
+            # FIXME: this is very slow. Do we need versioning functionality?
             # sm.repo.git.remote('add', 'origin', package.url)
             # sm.repo.git.fetch('--tags')
         except Exception as e:
