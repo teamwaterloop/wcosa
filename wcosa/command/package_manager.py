b9fd100768547e3c6eb0b8a438eb2460e1baf688
Dmytro Shynkevych <dm.shynk@gmail.com>
2018-02-04 01:42:12 -0500

    PEP8 compliance

--- wcosa/command/package_manager.py
+++ wcosa/command/package_manager.py
@@ -2,11 +2,15 @@
 A lightweight package management system for WCosa.
 *_many functions operate on lists of data (common scenario, better efficiency).
 """
-import git
+
 import json
+import git
+import os
 import re
-import os, sys
+import sys
+
 from wcosa.utils.output import write, writeln
+
 
 class Package:
     def __init__(self, name, url, branch, version, path):
@@ -15,46 +19,53 @@ class Package:
         self.branch = branch
         self.version = version
         self.path = path
-        self.name = (self.unqualified_name
-            + ('-' + self.branch if self.branch != 'master' else '')
-            + ('-' + self.version if self.version != 'master' else ''))
+        self.name = (self.unqualified_name +
+                     ('-' + self.branch if self.branch != 'master' else '') +
+                     ('-' + self.version if self.version != 'master' else ''))
 
     def __repr__(self):
-        return ("name: %s, url: %s, branch: %s, version: %s, path: %s" %
-                    (self.name, self.url, self.branch, self.version, self.path))
+        return ('name: %s, url: %s, branch: %s, version: %s, path: %s' %
+                (self.name, self.url, self.branch, self.version, self.path))
+
 
 class PackageFormatError(Exception):
     def __init__(self, package_string):
         self.package_string = package_string
 
     def __str__(self):
-        return "Bad package format: " + self.package_string
+        return 'Bad package format: ' + self.package_string
+
 
 class GitFetchException(Exception):
     def __init__(self, package):
         self.url = (package.url +
-                    (':' + package.branch if package.branch != 'master' else '')
+                    (':' + package.branch if package.branch != 'master'
+                     else '')
                     ('@' + package.version if package.version != 'master'
-                           else ''))
+                     else ''))
 
     def __str__(self):
-        return "Could not fetch submodule from %s" + package.url
+        return 'Could not fetch submodule from %s' + package.url
+
 
 class AlreadyInstalledException(Exception):
     def __init__(self, link_updated):
         self.link_updated = link_updated
+
 
 URL = r'(?P<url>https?://\S+/(?P<name>\S+))'
 GITHUB = r'(?P<github>[\w\-]+/(?P<name>[\w\-]+))'
 BRANCH = r'(:(?P<branch>[\w\-]+))?'
 VERSION = r'(@(?P<version>\S+))?'
 PATH = r'( as (?P<path>\S+))?'
-VALID_SCHEMAS = [re.compile('^\s*' + URL + BRANCH + VERSION + PATH + '\s*$'),
-                 re.compile('^\s*' + GITHUB + BRANCH + VERSION + PATH + '\s*$')]
+VALID_SCHEMAS = [re.compile('^' + URL + BRANCH + VERSION + PATH + '$'),
+                 re.compile('^' + GITHUB + BRANCH + VERSION + PATH + '$')]
+
 
 def package_dir_path(path):
     """Return package path to package install directory"""
     return path + '/.pkg'
+
 
 def package_string_parse_many(package_strings):
     """
@@ -76,7 +87,7 @@ def package_string_parse_many(package_strings):
                 break
         if not match:
             raise PackageFormatError(package_string)
-        if 'github' in groups: # only a group if matched with github short form
+        if 'github' in groups:  # only a group if matched with github format
             url = 'https://github.com/' + groups['github']
         else:
             url = groups['url']
@@ -87,6 +98,7 @@ def package_string_parse_many(package_strings):
         packages.append(Package(name, url, branch, version, path))
     return packages
 
+
 def package_list_read(pkgpath):
     """Read package list"""
     try:
@@ -95,10 +107,11 @@ def package_list_read(pkgpath):
     except:
         return []
 
+
 def package_list_write_many(pkgpath, packages):
     """Update package list with the given list of packages"""
     if not packages:
-        return # Nothing to write
+        return  # Nothing to write
     repo = package_repo_open(pkgpath)
     newentries = []
     updentries = []
@@ -108,7 +121,7 @@ def package_list_write_many(pkgpath, packages):
         for package in packages:
             if package.name in pkgnames:
                 index = pkgnames.index(package.name)
-                if package.path in pkglist[index]['paths']: # Paths may not match
+                if package.path in pkglist[index]['paths']:
                     continue
                 pkglist[index]['paths'].append(package.path)
                 updentries.append(package.name)
@@ -120,84 +133,89 @@ def package_list_write_many(pkgpath, packages):
         pkglistfile.seek(0)
         pkglistfile.write(json.dumps(pkglist))
     repo.index.add(['pkglist'])
-    if repo.is_dirty(): # Something has changed
-        repo.index.commit("Updated package list\n\n"
-                          + ("New: %s\n" % ', '.join(newentries)
-                              if newentries else "")
-                          + ("Updated: %s\n" % ', '.join(updentries)
-                              if updentries else ""))
+    if repo.is_dirty():  # Something has changed
+        repo.index.commit('Updated package list\n\n' +
+                          ('New: %s\n' % ', '.join(newentries)
+                           if newentries else '') +
+                          ('Updated: %s\n' % ', '.join(updentries)
+                           if updentries else ''))
+
 
 def package_repo_open(pkgpath):
     """Try to open package repo; initalize upon failure"""
     try:
         return git.Repo(pkgpath)
-    except: # If could not open, initialize
+    except:
         return package_repo_init(pkgpath)
+
 
 def package_repo_init(pkgpath):
     """Initialize package repo"""
-    write("Initializing package repository... ")
+    write('Initializing package repository... ')
     sys.stdout.flush()
     pkgrepo = git.Repo.init(pkgpath)
 
     with open(pkgpath + '/pkglist', 'w+') as pkglist:
-        pkglist.write('[]') # Start with empty package list
+        pkglist.write('[]')  # Start with empty package list
 
     pkgrepo.index.add(['pkglist'])
-    pkgrepo.index.commit("Initialized repository")
-    writeln("Done")
+    pkgrepo.index.commit('Initialized repository')
+    writeln('Done')
 
     return pkgrepo
 
+
 def package_link(path, package):
     """Link package directory from pkgpath to package.path"""
-    install_path = os.path.abspath(package_dir_path(path)+ '/' + package.name)
+    install_path = os.path.abspath(package_dir_path(path) + '/' + package.name)
     link_path = os.path.abspath(path + '/' + package.path)
     link_basedir = '/'.join(link_path.split('/')[:-1])
     try:
         os.mkdir(link_basedir)
     except:
-        pass # Already exists or failed (then next try will fail)
+        pass  # Already exists or failed (then next try will fail)
     try:
         os.symlink(install_path, link_path)
     except Exception as e:
-        try: # Maybe the path is already linked
+        try:  # Maybe the path is already linked
             current_path = os.readlink(link_path)
             if current_path == install_path:
-                return # Then we're done
+                return  # Then we're done
         except:
             pass
-        raise (type(e))("Could not link package: " + str(e))
+        raise (type(e))('Could not link package: ' + str(e))
+
 
 def _package_install_unsafe(path, package, pkgrepo, pkglist, pkgnames):
     """
-    NOT A PUBLIC INTERFACE: use package_install or package_install_many instead.
+    NOT A PUBLIC INTERFACE: use package_install[_many] instead.
 
     Try to install a package and forward exceptions to the caller.
     Will leave package repository in dirty state.
     Returns
     """
-    write("Installing %s... " % package.name)
+    write('Installing %s... ' % package.name)
     sys.stdout.flush()
     if package.name in pkgnames:
         index = pkgnames.index(package.name)
         if package.path in pkglist[index]['paths']:
-            writeln("Already installed.")
-            raise AlreadyInstalledException(link_updated = False)
+            writeln('Already installed.')
+            raise AlreadyInstalledException(link_updated=False)
         else:
-            write("Already installed, linking to %s... " % package.path)
+            write('Already installed, linking to %s... ' % package.path)
             sys.stdout.flush()
             package_link(path, package)
-            writeln("Linked.")
-            raise AlreadyInstalledException(link_updated = True)
+            writeln('Linked.')
+            raise AlreadyInstalledException(link_updated=True)
     # If the above did not return, we need to actually install the package
     try:
         sm = pkgrepo.create_submodule(package.name, package.name,
                                       url=package.url, branch=package.branch)
-    except: # Default message is cryptic
+    except:  # Default message is cryptic
         raise GitFetchException(package)
     package_link(path, package)
-    writeln("Installed.")
+    writeln('Installed.')
+
 
 def package_install(path, package, batch_mode=False, pkgrepo=None,
                     pkglist=None, pkgnames=None):
@@ -216,22 +234,23 @@ def package_install(path, package, batch_mode=False, pkgrepo=None,
     try:
         _package_install_unsafe(path, package, pkgrepo, pkglist, pkgnames)
         pkgrepo.index.add(['.gitmodules', package.name])
-        pkgrepo.index.commit("Installed " + package.name)
+        pkgrepo.index.commit('Installed ' + package.name)
         if not batch_mode:
             package_list_write_many(pkgpath, [package])
     except AlreadyInstalledException as e:
         return e.link_updated
-    except Exception as e: # Installation failed: rollback
+    except Exception as e:  # Installation failed, roll back
         try:
             sm = pkgrepo.submodule(package.name)
             sm.remove()
         except:
             pass
-        pkgrepo.git.clean('-fdX') # Remove all untracked files
-        writeln("Install aborted.")
+        pkgrepo.git.clean('-fdX')  # Remove all untracked files
+        writeln('Install aborted.')
         writeln(str(e))
         return False
     return True
+
 
 def package_install_many(path, packages):
     """Install a list of packages"""
@@ -244,14 +263,15 @@ def package_install_many(path, packages):
 
     for package in packages:
         if package_install(path, package, True, pkgrepo, pkglist, pkgnames):
-            installed_packages.append(package) # To be written to database
+            installed_packages.append(package)  # To be written to database
     if installed_packages:
         package_list_write_many(pkgpath, installed_packages)
+
 
 def package_update_all(path):
     """Update all installed packages"""
     repo = package_repo_open(package_dir_path(path))
     for sm in repo.submodules:
-        write("Updating %s... " % sm.name)
+        write('Updating %s... ' % sm.name)
         sm.update()
-        writeln("Done.")
+        writeln('Done.')
