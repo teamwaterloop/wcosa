82469b964bfa302844f169a276e0ad2f3f791d8b
Deep Dhillon <deep.dhill6@gmail.com>
2018-05-15 09:25:31 -0400

    made Makefile cross compatible and fixed some build issues

--- cmd/wio/utils/configuration.go
+++ cmd/wio/utils/configuration.go
@@ -8,43 +8,18 @@
 package utils
 
 import (
-    "bufio"
     "strings"
-    "fmt"
     "gopkg.in/yaml.v2"
-    "os"
-
+    "wio/cmd/wio/utils/io"
 )
 
-// Adds spacing and other formatting for project configuration
-func writeProjectConfig(lines []string, path string) error {
-    file, err := os.Create(path)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
-
-    w := bufio.NewWriter(file)
-    for _, line := range lines {
-        tokens := strings.Split(line, "\n")
-        for _, token := range tokens {
-            if strings.Contains(token, "targets:") ||
-                (strings.Contains(token, "libraries:") && !strings.Contains(token, "#   libraries:")) {
-                fmt.Fprint(w, "\n")
-            }
-            fmt.Fprintln(w, token)
-        }
-    }
-
-    return w.Flush()
-}
 
 // Write configuration for the project with information on top and nice spacing
 func PrettyPrintConfig(projectConfig interface{}, filePath string) (error) {
-    appInfoPath := "templates" + Sep + "config" + Sep + "app-helper.txt"
-    pkgInfoPath := "templates" + Sep + "config" + Sep + "pkg-helper.txt"
-    targetsInfoPath := "templates" + Sep + "config" + Sep + "targets-helper.txt"
-    dependenciesInfoPath := "templates" + Sep + "config" + Sep + "dependencies-helper.txt"
+    appInfoPath := "templates" + io.Sep + "config" + io.Sep + "app-helper.txt"
+    pkgInfoPath := "templates" + io.Sep + "config" + io.Sep + "pkg-helper.txt"
+    targetsInfoPath := "templates" + io.Sep + "config" + io.Sep + "targets-helper.txt"
+    dependenciesInfoPath := "templates" + io.Sep + "config" + io.Sep + "dependencies-helper.txt"
 
     var ymlData []byte
     var appInfoData []byte
@@ -57,16 +32,16 @@ func PrettyPrintConfig(projectConfig interface{}, filePath string) (error) {
     if ymlData, err = yaml.Marshal(projectConfig); err != nil {
         return err
     }
-    if appInfoData, err = AssetIO.ReadFile(appInfoPath); err != nil {
+    if appInfoData, err = io.AssetIO.ReadFile(appInfoPath); err != nil {
         return err
     }
-    if pkgInfoData, err = AssetIO.ReadFile(pkgInfoPath); err != nil {
+    if pkgInfoData, err = io.AssetIO.ReadFile(pkgInfoPath); err != nil {
         return err
     }
-    if targetsInfoData, err = AssetIO.ReadFile(targetsInfoPath); err != nil {
+    if targetsInfoData, err = io.AssetIO.ReadFile(targetsInfoPath); err != nil {
         return err
     }
-    if dependenciesInfoData, err = AssetIO.ReadFile(dependenciesInfoPath); err != nil {
+    if dependenciesInfoData, err = io.AssetIO.ReadFile(dependenciesInfoPath); err != nil {
         return err
     }
 
@@ -108,7 +83,7 @@ func PrettyPrintConfig(projectConfig interface{}, filePath string) (error) {
         finalString += currLine + "\n"
     }
 
-    err = NormalIO.WriteFile(filePath, []byte(finalString))
+    err = io.NormalIO.WriteFile(filePath, []byte(finalString))
 
     return err
 }
